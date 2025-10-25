cd9693ce3b13588994a354f09cef10b10d8f8e3e
“Abdullah <2960862a@student.gla.ac.uk>
2025-10-25 15:37:43 +0100

    Add ActivityEntity model with Freezed for immutable data handling

---
+++ lib/modules/activities/entity/activity_entity.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'activity_entity.freezed.dart';
+part 'activity_entity.g.dart';
+
+
+@freezed
+abstract class ActivityEntity with _$ActivityEntity {
+  const factory ActivityEntity({
+    required int id,
+    required DateTime createdAt,
+    required String name,
+    String? description,
+    int? cost,
+    String? website,
+    String? location,
+    int? minGroupSize,
+    int? maxGroupSize,
+  }) = _ActivityEntity;
+
+  factory ActivityEntity.fromJson(Map<String, dynamic> json) => _$ActivityEntityFromJson(json);
+}
