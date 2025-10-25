aa87a0fb816a637fb00ad0e6c4605b46ffb9c9d9
“Abdullah <2960862a@student.gla.ac.uk>
2025-10-25 21:38:44 +0100

    Fix layout issue by closing missing widget in ActivityDetailPage

--- lib/modules/activities/activity_detail_page.dart
+++ lib/modules/activities/activity_detail_page.dart
@@ -179,6 +179,7 @@ class ActivityDetailPage extends ConsumerWidget {
                     ),
                   ),
                 ),
+                ),
               ),
             ],
           );
